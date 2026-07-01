a15fb5e2a76e2462ddea549efe87fdadfe1fcda9
ameer021 <86469150+ameer021@users.noreply.github.com>
2026-07-01 14:56:57 +0800

    Add functions to calculate total and print receipt

--- week_5/utils.py
+++ week_5/utils.py
@@ -1,1 +1,12 @@
+def calculate_total(coffee, tea, sandwich):
+    return (coffee * 8.50) + (tea * 6.00) + (sandwich * 12.00)
 
+
+def print_receipt(customer_name, coffee, tea, sandwich, total):
+    print("\n===== RECEIPT =====")
+    print(f"Customer : {customer_name}")
+    print(f"Coffee   : {coffee}")
+    print(f"Tea      : {tea}")
+    print(f"Sandwich : {sandwich}")
+    print("-------------------")
+    print(f"Total = RM {total:.2f}")
